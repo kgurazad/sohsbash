54a3ab73d4437a29efe34ee6a53a460245026c88
Karan Gurazada <karangurazada@gmail.com>
2018-11-24 13:07:30 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const pug = require('pug');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+mongoose.connect(process.env.DB);
+
+const quote = require(__dirname + '/models/quote');
+// const mod = require(__dirname + '/models/mod');
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/static/:file', function (req, res) {
+	res.sendFile(__dirname + '/static/' + req.params.file);
+});
+app.get('/new', function (req, res) {
+	res.sendFile(__dirname + '/views/new.html');
+});
+app.get('/search', function (req, res) {
+	res.sendFile(__dirname + '/views/search.html'); // change this around later
+});
+app.post('/search', function (req, res) {
+	try {
+		var queryParams = {};
+		if (req.body.quote_id) {
+			var quotes_split = req.body.quote_id.split(' ');
+			for (var q in quotes_split) {
+				quotes_split[q] = Number(quotes_split[q]);
+			}
+			queryParams.id = {$in: quotes_split};
+		}
+		if (req.body.content) {
+			queryParams.content = {$regex: new RegExp(req.body.content, 'g')};
+		}
+		if (req.body.tags) {
+			var quotes_split = req.body.tags.split(' ');
+			queryParams.tags = {$in: quotes_split};
+		}
+		if (req.body.upvotes) {
+			queryParams.upvotes = {$gte: Number(req.body.upvotes)};
+		}
+		if (req.body.downvotes) {
+			queryParams.downvotes = {$gte: Number(req.body.downvotes)};
+		}
+		quote.search(queryParams, function (err, quotes) {
+			res.send(JSON.stringify(quotes));
+		});
+	} catch (e) {
+		console.error(e);
+		res.sendStatus(400);
+	}
+})
+app.post('/new', function (req, res) {
+	quote.newQuote(req.body.content, req.body.notes, req.body.tags);
+});
+app.post('/upvote', function (req, res) {
+	console.log(req.body.takeback);
+	quote.upvote(req.body.id, req.body.takeback);
+});
+app.post('/downvote', function (req, res) {
+	quote.downvote(req.body.id, req.body.takeback);
+});
+
+app.listen(process.env.PORT || 2020);
---
+++ models/quote.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const quoteSchema = new mongoose.Schema({
+	id: Number,
+	content: String,
+	notes: String,
+	tags: [String],
+	upvotes: Number,
+	downvotes: Number,
+	datePosted: Date,
+	verified: Boolean,
+	deleted: Boolean
+});
+var quoteModel = mongoose.model("quote", quoteSchema);
+
+exports.newQuote = function (content, notes, tags) {
+	var splitTags = tags.split(' ');
+	quoteModel.count({}, function (err, count) {
+		var quote = new quoteModel({
+			id: count,
+			content: content,
+			notes: notes,
+			tags: splitTags,
+			upvotes: 0,
+			downvotes: 0,
+			datePosted: new Date(),
+			verified: /*false*/ true, // lol fix this when we make the mod portal pl0x
+			deleted: false
+		});
+		quote.save();
+	});
+}
+
+exports.upvote = function (id, takeback = false) {
+	var n = 1;
+	if (takeback === true) {
+		n = -1;
+	}
+	quoteModel.findOneAndUpdate({id: id}, {$inc: {upvotes: n}}, function (err, quote) {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
+
+exports.downvote = function (id, takeback) {
+	var n = 1;
+	if (takeback === true) {
+		console.log('uh oh')
+		n = -1;
+	}
+	quoteModel.findOneAndUpdate({id: id}, {$inc: {downvotes: n}}, function (err, quote) {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
+
+exports.setVerified = function (id, verified) {
+	quoteModel.findOneAndUpdate({id: id}, {$set: {verified: verified}}, function (err, quote) {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
+
+exports.setDeleted = function (id, deleted) {
+	quoteModel.findOneAndUpdate({id: id}, {$set: {deleted: deleted}}, function (err, quote) {
+		if (err) {
+			console.error(err);
+		}
+	});
+}
+
+exports.search = function (query, cb) {
+	quoteModel.find(query, cb); // yay design patterns
+}
