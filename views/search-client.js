771a427b87012481c53e1ef7333b0f4bb5c1c328
Karan Gurazada <karangurazada@gmail.com>
2018-11-26 00:26:57 -0800

    add search window ui

--- views/search-client.js
+++ views/search-client.js
@@ -12,7 +12,11 @@ var search = function () {
 
 var render = function (quotes) {
 	for (var quote of quotes) {
-		var html = '<div class="quote section card"><nav aria-label="breadcrumb"><ol class="breadcrumb"><li class="breadcrumb-item">Quote #' + quote.id + '</li>';
+		var html = '<div class="quote section card"><nav aria-label="breadcrumb">' +
+			'<ol class="breadcrumb"><li class="breadcrumb-item">#' + quote.id + '</li>' + 
+			'<li class="breadcrumb-item">' + quote.datePosted + '</li>' +
+			'<span class="vote" id="up_' + quote.id + '">UP</span> ' + (quote.upvotes - quote.downvotes) + '/' + (quote.upvotes + quote.downvotes) +
+			' <span class="vote" id="down_' + quote.id + '"DOWN</span></li>';
 		for (var tag of quote.tags) {
 			html += '<li class="breadcrumb-item">' + tag + '</li>';
 		}
