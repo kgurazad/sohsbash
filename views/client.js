98e949a98ef7e8dd84a3259c5e19773cfc11e963
Karan Gurazada <karangurazada@gmail.com>
2018-11-26 01:12:11 -0800

    add search window ui

--- views/client.js
+++ views/client.js
@@ -1,3 +1,15 @@
+var vote = function (action, id, takeback) {
+	if (action === 'up') {
+		$.post('/upvote', {id: id, takeback: takeback}, function (data, result) {
+			alert(data);
+		});
+	} else {
+		$.post('/downvote', {id: id, takeback: takeback}, function (data, result) {
+			alert(data);
+		});
+	}
+}
+
 var search = function () {
 	$.post('/search', {
 			quote_id: $('#quote_id').val(),
@@ -17,7 +29,7 @@ var render = function (quotes) {
 		var html = '<div class="quote section card"><nav aria-label="breadcrumb">' +
 			'<ol class="breadcrumb"><li class="breadcrumb-item">#' + quote.id + '</li>' + 
 			'<li class="breadcrumb-item">' + quote.datePosted + '</li><li class="breadcrumb-item">' +
-			'<span class="vote" id="up_' + quote.id + '">UP</span> <code>' + quote.upvotes + '/' + (quote.upvotes - quote.downvotes) + '/' + quote.downvotes +
+			'<span class="vote" id="up_' + quote.id + '">UP</span> <code id="indicator_' + quote.id + '">' + quote.upvotes + '/' + (quote.upvotes - quote.downvotes) + '/' + quote.downvotes +
 			'</code> <span class="vote" id="down_' + quote.id + '">DOWN</span></li>';
 		for (var tag of quote.tags) {
 			html += '<li class="breadcrumb-item">' + tag + '</li>';
@@ -36,5 +48,17 @@ $(document).ready(function () {
 	search();
 	$('#searchButton').on('click', function () {
 		search();
+	});
+	$('.vote').on('click', function () {
+		var sp = $(this).attr(id).split('_');
+		var action = sp[0];
+		var id = Number(sp[1]);
+		if ($(this).hasClass('takeback')) {
+			$(this).removeClass('takeback');
+			vote(action, id, true);
+		} else {
+			$(this).addClass('takeback');
+			vote(action, id, false);
+		}
 	});
 });
