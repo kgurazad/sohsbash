3df55a229b041299e22cbd11ccb892ac8f50f36c
Karan Gurazada <karangurazada@gmail.com>
2018-12-05 13:24:29 -0800

    add mod portal

--- models/mod.js
+++ models/mod.js
@@ -12,7 +12,7 @@ exports.auth = function (username, password, cb) {
 			return;
 		}
 		console.log(mod + ' <- mod object');
-		if (mod.username === usernamee && mod.password === password) {
+		if (mod.username === username && mod.password === password) {
 			cb(true);
 		} else {
 			cb(false);
